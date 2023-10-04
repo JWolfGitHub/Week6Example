ee4dbe7fc4410b662460db1c2ce4969c2c76921b
JWolfGitHub <57966644+JWolfGitHub@users.noreply.github.com>
2023-10-04 14:52:54 -0400

    Add project files.

---
+++ OurFirstApp/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OurFirstApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public string Message { get; set; }
+
+        public string Lab6WantToLearn { get; set; }
+
+        public void OnGet()
+        {
+            Message = $"Hello World ~  {DateTime.Now.ToLongTimeString()}  ~  {DateTime.Now.ToLongDateString()}";
+
+            Lab6WantToLearn = "I've got a decent foundational understanding of using HTML and CSS in web development. I need to learn A LOT MORE about back-end development, providing the functionality to web pages. I also need to learn more of the terminology. Sometimes the videos and downloadable exercises are confusing because I'm not 100% sure what they mean.";
+        }
+    }
+}
